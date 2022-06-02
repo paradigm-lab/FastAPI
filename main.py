da674a4970fff5a06ef0a0282dbaeec93baf9a36
collins <collinsboniface1000@gmail.com>
2022-06-02 22:56:45 +0300

    Explored about the FastAPI and dynamic routing and parameter routing

--- main.py
+++ main.py
@@ -9,10 +9,23 @@ app = FastAPI()
 # @app is called path operation decorator
 @app.get("/")
 def index():
+    return {"data": {"data": "Blog List"}}
 
-    return {"data": {"name": "User1"}}
+
+@app.get("/blog/unpublished")
+def unpublished():
+    return {"data": "all unpublished blogs"}
 
 
-@app.get("/about")
-def about():
-    return {"data": "About Page"}
+# Dynamic routing
+# The type definition is all done by Pydantic library
+@app.get("/blog/{id}")
+def show(id: int):
+    return {"data": id}
+
+
+@app.get("/blog/{id}/comments")
+def comments(id):
+    # Fetch comments of blog with id = id
+    return {"data": {"1", "2"}}
+
