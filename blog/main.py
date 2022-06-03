9735bbc9cc8dedb68410e4d5b478eea88123c16d
collins <collinsboniface1000@gmail.com>
2022-06-03 18:23:02 +0300

    Imported the schemas and get referrence to the actuall properties

--- blog/main.py
+++ blog/main.py
@@ -1,15 +1,9 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from . import schemas
 
 app = FastAPI()
 
 
-# Pydantic model
-class Blog(BaseModel):
-    title: str
-    body: str
-
-
 @app.post("/blog")
-def create(request: Blog):
+def create(request: schemas.Blog):
     return {"title": request.title, "body": request.body}
