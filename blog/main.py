92df26a451a634e3837970f874b743a5e6c0eca0
collins <collinsboniface1000@gmail.com>
2022-06-03 18:06:20 +0300

    Created a pydantic model for the blog

---
+++ blog/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+# Pydantic model
+class Blog(BaseModel):
+    title: str
+    body: str
+
+
+@app.post("/blog")
+def create(request: Blog):
+    return {"title": request.title, "body": request.body}
