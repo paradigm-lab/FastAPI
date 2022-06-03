d9c13b7c8df8de2aad8d5706f7e0c6a169f647d7
collins <collinsboniface1000@gmail.com>
2022-06-03 19:32:24 +0300

    Created the database session and used the Blog schema to post the values into the database

--- blog/main.py
+++ blog/main.py
@@ -1,12 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from . import schemas, models
-from . database import engine
+from . database import engine, SessionLocal
+from sqlalchemy.orm import Session
+
 
 app = FastAPI()
 
+# Migrating all the tables
 models.Base.metadata.create_all(engine)
 
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+# Store blog to Database
 @app.post("/blog")
-def create(request: schemas.Blog):
-    return {"title": request.title, "body": request.body}
+def create(request: schemas.Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title=request.title, body=request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
+
