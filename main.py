fb4341febc8b3f475592e1c5dc2eb75de7271749
collins <collinsboniface1000@gmail.com>
2022-06-05 15:03:56 +0300

    Create a new path operation for the posts

--- main.py
+++ main.py
@@ -2,7 +2,20 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+# The Request order matters
+# Example: Request GET method URL: "/"
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+
+# Path Operation or Route
+@app.get("/")   # Decorator: We get the HTTP method and ("") path
+def root():     # Function Will go to consist all the logic for performing the specific task
+
+    return {"message": "Welcome to my API"}   # FastAPI will automatically converter the dictionary to a JSON format
+
+
+@app.get("/posts")
+def get_posts():
+
+    return {"data": "This is your posts"}
+
+
