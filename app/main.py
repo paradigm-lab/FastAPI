190c7ad03715d0d34bc451230e214e4ece29de6b
collins <collinsboniface1000@gmail.com>
2022-07-04 10:45:25 +0300

    Created a new Users route for create method

--- app/main.py
+++ app/main.py
@@ -202,3 +202,11 @@ def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends
     return post_query.first()
 
 
+@app.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    new_user = models.User(**user.dict())
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
