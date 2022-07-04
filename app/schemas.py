b4becb0424bef98f00c95410c8ed16dea31e9c42
collins <collinsboniface1000@gmail.com>
2022-07-04 10:01:56 +0300

    Created a new Response Schema in our schema file

--- app/schemas.py
+++ app/schemas.py
@@ -1,7 +1,8 @@
 from pydantic import BaseModel
+from datetime import datetime
 
 
-# Extending Pydatic Model
+# Pydatic Model Extend BaseModel
 # Schema
 class PostBase(BaseModel):
     title: str
@@ -12,3 +13,12 @@ class PostBase(BaseModel):
 # Using Inheritance approach
 class PostCreate(PostBase):
     pass
+
+
+# Response schema
+class Post(PostBase):   # Inherit the PostBase
+    id: int
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
