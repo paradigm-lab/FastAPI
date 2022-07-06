f436fd2b04174ca72ca4afafd91bc6b2c7a61f86
collins <collinsboniface1000@gmail.com>
2022-07-06 12:17:45 +0300

    Updated the Expiration time and ge_current_user f(x) to access the database and return the User obejct

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,8 +1,9 @@
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
-from . import schemas
+from . import schemas, database, models
 from fastapi import Depends, status, HTTPException
 from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
 
 outh2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
@@ -13,7 +14,7 @@ outh2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 # To get a string like this run "openssl rand -hex 32"
 SECRET_KEY = "6b02679b2bda59ee77826b19dfbeda795fa77715f27053834d25e73cbb154673"
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+ACCESS_TOKEN_EXPIRE_MINUTES = 60        # Expiration token should only be withing 30 MINUTES
 
 
 def create_access_token(data: dict):
@@ -50,10 +51,14 @@ def verify_access_token(token: str, credentials_exception):
     return token_data   # id
 
 
-def get_current_user(token: str = Depends(outh2_scheme)):
+def get_current_user(token: str = Depends(outh2_scheme), db: Session = Depends(database.get_db)):
     credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
                                           detail=f"Could not validate credentials",
                                           headers={"WWW-Authenticate": "Bearer"})
 
-    return verify_access_token(token, credentials_exception)
+    token = verify_access_token(token, credentials_exception)
+
+    user = db.query(models.User).filter(models.User.id == token.id).first()
+
+    return user
 
