7ff1628928ac72d94623bd4e0a0ac77f5cd29e7a
collins <collinsboniface1000@gmail.com>
2022-07-07 19:29:01 +0300

    Database layer testing on delete action

--- Database/query.sql
+++ Database/query.sql
@@ -2,4 +2,12 @@ SELECT * FROM posts
 WHERE user_id = 13;
 
 
+SELECT * FROM posts;
 
+
+-- Query to delete users to test on delete action which was set to CASCADE
+DELETE FROM users 
+WHERE id = 15;
+
+
+SELECT * FROM posts;
