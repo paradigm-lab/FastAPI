2f3768d3e9fbba8f9de743c8562a1ac693f8047e
collins <collinsboniface1000@gmail.com>
2022-07-08 09:03:23 +0300

    Telling env class to check for the environment variables in the .env file

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ from pydantic import BaseSettings
 
 
 # Performing Validation for the Environment variables
-class Settings(BaseSettings):   # Pydantic is going to handle the case sensitive
+class Settings(BaseSettings):   # Pydantic is going to handle the case insensitive
     database_hostname: str
     database_port: str
     database_password: str
@@ -11,6 +11,9 @@ class Settings(BaseSettings):   # Pydantic is going to handle the case sensitive
     secret_key: str
     algorithm: str
     access_token_expire_minutes: int
+
+    class Config:
+        env_file = ".env"
 
 
 settings = Settings()   # Creating an instance of the Settings() class
