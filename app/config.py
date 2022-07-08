d09f1156f375bd87c51a2d09794cfacaed9a3c3d
collins <collinsboniface1000@gmail.com>
2022-07-08 08:54:07 +0300

    Providing the environment variables need for our API

--- app/config.py
+++ app/config.py
@@ -1,10 +1,16 @@
 from pydantic import BaseSettings
 
 
-class Settings(BaseSettings):
-    path: str
-    language: str
-    shell: str
+# Performing Validation for the Environment variables
+class Settings(BaseSettings):   # Pydantic is going to handle the case sensitive
+    database_hostname: str
+    database_port: str
+    database_password: str
+    database_name: str
+    database_username: str
+    secret_key: str
+    algorithm: str
+    access_token_expire_minutes: int
 
 
-settings = Settings()
+settings = Settings()   # Creating an instance of the Settings() class
