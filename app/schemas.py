be702d1c29d09ac2cbcf9676c4c50158eda1fc7f
collins <collinsboniface1000@gmail.com>
2022-07-13 11:10:30 +0300

    Updated the Scheme according to the response model

--- app/schemas.py
+++ app/schemas.py
@@ -39,6 +39,14 @@ class Post(PostBase):   # Inherit the PostBase
         orm_mode = True
 
 
+class PostOut(BaseModel):
+    Post: Post
+    votes: int
+
+    class Config:
+        orm_mode = True
+
+
 class UserCreate(BaseModel):
     email: EmailStr
     password: str
