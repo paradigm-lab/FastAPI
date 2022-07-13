02628f6aa2b895747eba42570b14a6d498e12404
collins <collinsboniface1000@gmail.com>
2022-07-13 11:15:06 +0300

    Updated the GET Post by id with a new query

--- app/routers/post.py
+++ app/routers/post.py
@@ -63,7 +63,7 @@ def create_posts(post: schemas.PostCreate,
 
 
 # Path parameter(id)
-@router.get("/{id}", response_model=schemas.Post)
+@router.get("/{id}", response_model=schemas.PostOut)
 def get_post(id: int, db: Session = Depends(get_db), current_user: id = Depends(oauth2.get_current_user)):
     """
     post = find_post(id)
@@ -78,7 +78,11 @@ def get_post(id: int, db: Session = Depends(get_db), current_user: id = Depends(
     # post = cursor.fetchone()
 
     # We don't use the all() method because it's going to keep searching for all the data instead we use first() method.
-    post = db.query(models.Post).filter(models.Post.id == id).first()  # More efficient
+    # post = db.query(models.Post).filter(models.Post.id == id).first()  #More efficient
+
+    post = db.query(models.Post, func.count(models.Vote.post_id).label("votes")).join(
+        models.Vote, models.Vote.post_id == models.Post.id, isouter=True).group_by(
+        models.Post.id).filter(models.Post.id == id).first()
 
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
