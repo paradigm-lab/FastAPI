cc13dc0776db7ae7d1e96dcb11662ba434099237
collin <collin.collindigm.com>
2022-07-19 09:52:39 +0300

    Moved the fixture into the conftest file

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,19 +1,6 @@
-import pytest
 from jose import jwt
 from app import schemas
-from .database import session, client
 from app.config import settings
-
-
-@pytest.fixture(scope="function")
-def test_user(client):
-    user_data = {"email": "developer@fastapi.com", "password": "developer"}
-    res = client.post("/users/", json=user_data)
-    assert res.status_code == 201
-    new_user = res.json()
-    new_user["password"] = user_data["password"]
-    # print(res.json)
-    return new_user
 
 
 def test_root(client):
