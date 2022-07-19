be94e6a7a802ef8163472df4089776207c577295
collin <collin.collindigm.com>
2022-07-19 11:04:44 +0300

    Wrote the get all posts with the authorized client and the test_posts fixture

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -1,4 +1,9 @@
-def get_all_posts(client, test_user):
-    client.post()
+from typing import List
+from app import schemas
 
+
+def test_get_all_posts(authorized_client, test_posts):
+    res = authorized_client.get("/posts")
+    assert len(res.json()) == len(test_posts)
+    assert res.status_code == 200
 
