3e0596e676607ce969e60c2eccfc55ffc2e5e12a
collin <collin.collindigm.com>
2022-07-19 09:43:23 +0300

    Decoding the token and checking for the user id and token type

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,6 +1,8 @@
 import pytest
+from jose import jwt
 from app import schemas
 from .database import session, client
+from app.config import settings
 
 
 @pytest.fixture(scope="function")
@@ -10,6 +12,7 @@ def test_user(client):
     assert res.status_code == 201
     new_user = res.json()
     new_user["password"] = user_data["password"]
+    # print(res.json)
     return new_user
 
 
@@ -28,6 +31,15 @@ def test_create_user(client):
 
 
 def test_login_user(client, test_user):
+    # Getting access to our login route
     res = client.post("/login", data={"username": test_user["email"], "password": test_user["password"]})
-    print(res.json())
+    login_res = schemas.Token(**res.json())
+
+    # Decoding the token
+    payload = jwt.decode(login_res.access_token, settings.secret_key, algorithms=[settings.algorithm])
+    user_id = payload.get("user_id")
+
+    # Checking for validation for our user and the token
+    assert user_id == test_user["id"]
+    assert login_res.token_type == "bearer"
     assert res.status_code == 200
