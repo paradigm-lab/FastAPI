ad49eaf0af15202a20f9a39143fdc3e273fa99f3
collin <collin.collindigm.com>
2022-07-19 10:08:00 +0300

    Provided a formated string

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -26,7 +26,7 @@ def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session =
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f"Invalid Credentials")
 
     if not utils.verify(user_credentials.password, user.password):
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid Credentials")
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f"Invalid Credentials")
 
     # Create a token
     access_token = oauth2.create_access_token(data={"user_id": user.id})
