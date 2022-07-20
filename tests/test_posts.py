a2433445d422e3a6ef099d159ed1045580bdcfee
collin <collin.collindigm.com>
2022-07-20 06:31:27 +0300

    Provided test properties for getting posts

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -16,3 +16,25 @@ def test_get_all_posts(authorized_client, test_posts):
     assert len(res.json()) == len(test_posts)
     assert res.status_code == 200
 
+
+def test_unauthorized_user_get_all_posts(client, test_posts):
+    res = client.get("/posts/")
+    assert res.status_code == 401
+
+
+def test_unauthorized_user_get_one_post(client, test_posts):
+    res = client.get(f"/posts/{test_posts[0].id}")
+    assert res.status_code == 401
+
+
+def test_get_one_post_not_exist(authorized_client, test_posts):
+    res = authorized_client.get(f"/posts/1000000")
+    assert res.status_code == 404
+
+
+def test_get_one_post(authorized_client, test_posts):
+    res = authorized_client.get(f"/posts/{test_posts[0].id}")
+    post = schemas.PostOut(**res.json())
+    assert post.Post.id == test_posts[0].id
+    assert post.Post.content == test_posts[0].content
+    assert post.Post.title == test_posts[0].title
